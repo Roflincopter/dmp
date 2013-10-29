c8d4e7a3d8c123bdf06b160fb1826267ba176b5f
Dennis Brentjes <d.brentjes@gmail.com>
2013-10-29 21:09:07 +0100

    Moved server and port to the positional arguments

--- dmp-client/main.cpp
+++ dmp-client/main.cpp
@@ -20,19 +20,27 @@ int main(int argc, char* argv[]) {
     boost::program_options::options_description desc;
     desc.add_options()
             ("help", "produce help message")
-            ("server", value<std::string>(), "Server hostanme")
-            ("port", value<uint16_t>(), "Destination port")
-            ("name", value<std::string>(), "Your screen name")
-            ;
+            ("server", value<std::string>(), "The server you want to connect to")
+            ("port", value<uint16_t>(), "The port you want to connect to")
+            ("name", value<std::string>(), "Your screen name");
+
+    boost::program_options::positional_options_description pos_desc;
+    pos_desc.add("server", 1).add("port", 1);
 
     boost::program_options::variables_map vm;
-    boost::program_options::store(parse_command_line(argc, argv, desc), vm);
+    boost::program_options::store(boost::program_options::command_line_parser(argc, argv).options(desc).positional(pos_desc).run(), vm);
     boost::program_options::notify(vm);
-
 
     dmp::Connection conn = dmp::connect(vm["server"].as<std::string>(), vm["port"].as<uint16_t>());
 
-    DmpClient client(vm["name"].as<std::string>(), std::move(conn));
+    std::string name;
+    if(!vm["name"].empty()) {
+        name = vm["name"].as<std::string>();
+    } else {
+        name = boost::asio::ip::host_name();
+    }
+
+    DmpClient client(name, std::move(conn));
     //this also starts the asio eventloop
 
     return 0;
