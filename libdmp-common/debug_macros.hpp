24a4f346fdc763487f99e7e9b15d1efb93b509dd
Dennis Brentjes <d.brentjes@gmail.com>
2014-05-01 14:00:54 +0200

    Added some debug macros to help in identifying what outputs what

---
+++ libdmp-common/debug_macros.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <iostream>
+
+#define DEBUG_COUT std::cout << __FILE__ << ":" << __LINE__ << "(" << __FUNCTION__ << "): "
+#define DEBUG_CERR std::cerr << __FILE__ << ":" << __LINE__ << "(" << __FUNCTION__ << "): "
