832d19ef51433683581b567b3388fbc3533460ea
Dennis Brentjes <d.brentjes@gmail.com>
2014-11-05 14:11:55 +0100

    Fixes a template specifier in gcc.

--- dmp-client/delegator.hpp
+++ dmp-client/delegator.hpp
@@ -28,13 +28,13 @@ struct Delegator : protected Delegator_impl_gen<Delegates...>
 	template <typename Delegate>
 	void add_delegate(std::weak_ptr<Delegate> delegate)
 	{
-		Delegator_impl<Delegate>::template observers.push_back(delegate);
+		Delegator_impl<Delegate>::observers.template push_back(delegate);
 	}
 
 	template <typename Delegate>
 	void remove_delegates(std::weak_ptr<Delegate> delegate)
 	{
-		auto delegates = Delegator_impl<Delegate>::template observers;
+		auto delegates = Delegator_impl<Delegate>::observers;
 		if(auto it = delegates.find(delegate.lock()) != delegates.end())
 		{
 			delegates.erase(it);
@@ -44,7 +44,7 @@ struct Delegator : protected Delegator_impl_gen<Delegates...>
 	template <typename Delegate, typename U, typename... Args>
 	void call_on_delegates(U member_fun, Args... args)
 	{
-		for(auto&& delegate : Delegator_impl<Delegate>::template observers)
+		for(auto&& delegate : Delegator_impl<Delegate>::observers)
 		{
 			auto s = delegate.lock();
 			((*s).*member_fun)(args...);
