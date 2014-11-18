f194978acca5c46d8b26b77f30ac9073126a4ec8
Dennis Brentjes <d.brentjes@gmail.com>
2014-11-18 14:05:51 +0100

    Fixes the FindTagLib.cmake

--- libdmp-library/src/CMakeModules/FindTagLib.cmake
+++ libdmp-library/src/CMakeModules/FindTagLib.cmake
@@ -65,13 +65,13 @@ ELSE()
         ${INCLUDE_INSTALL_DIR}
       )
 
-      #find_library_with_debug(TAGLIB_LIBRARIES
-      #  WIN32_DEBUG_POSTFIX d
-      #  NAMES tag
-      #  PATHS
-      #  ${KDE4_LIB_DIR}
-      #  ${LIB_INSTALL_DIR}
-      #)
+      find_library(TAGLIB_LIBRARIES
+        WIN32_DEBUG_POSTFIX d
+        NAMES tag
+        PATHS
+        ${KDE4_LIB_DIR}
+        ${LIB_INSTALL_DIR}
+      )
       
       find_package_handle_standard_args(Taglib DEFAULT_MSG 
                                         TAGLIB_INCLUDES TAGLIB_LIBRARIES)
