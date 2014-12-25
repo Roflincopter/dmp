474ff8dcb784a23ebde5bfe27762d86680d08d62
Dennis Brentjes <D.brentjes@gmail.com>
2014-12-25 08:55:26 -0800

    Fixes the OSX build

--- dmp-server/server_exceptions.hpp
+++ dmp-server/server_exceptions.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <stdexcept>
+#include <string>
 
 struct InvalidClientEndpoint : public std::runtime_error {
 	InvalidClientEndpoint(std::string what = "ClientEndpoint was invalid");
